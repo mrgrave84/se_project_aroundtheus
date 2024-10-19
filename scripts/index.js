b15ee726d66f3d99ca14d3d59adb75523e0f4dea
Evgeny Mogilevskiy <mrgrave84@gmail.com>
2024-10-19 16:11:40 +0300

    submit project for first review

--- scripts/index.js
+++ scripts/index.js
@@ -30,19 +30,19 @@ const cardTemplate = document
   .content.querySelector(".card");
 
 const cardListEl = document.querySelector(".cards__list");
-const profileEditModal = document.querySelector("#profile-edit-modal");
-const addCardModal = document.querySelector("#add-card-modal");
-const previewImageModal = document.querySelector("#preview-image-modal");
-const profileEditModalForm = profileEditModal.querySelector(".modal__form");
-const addCardModalForm = addCardModal.querySelector(".modal__form");
+const profileEditModal = document.querySelector("#profile-edit-popup");
+const addCardModal = document.querySelector("#add-card-popup");
+const previewImageModal = document.querySelector("#preview-image-popup");
+const profileEditModalForm = profileEditModal.querySelector(".popup__form");
+const addCardModalForm = addCardModal.querySelector(".popup__form");
 
 // Buttons
 const profileEditButton = document.querySelector("#profile-edit-button");
-const profileModalCloseButton = profileEditModal.querySelector(".modal__close");
+const profileModalCloseButton = profileEditModal.querySelector(".popup__close");
 const addCardButton = document.querySelector(".profile__add-button");
-const addCardModalCloseButton = addCardModal.querySelector(".modal__close");
+const addCardModalCloseButton = addCardModal.querySelector(".popup__close");
 const previewImageModalCloseButton =
-  previewImageModal.querySelector(".modal__close");
+  previewImageModal.querySelector(".popup__close");
 
 // Elements
 const profileTitle = document.querySelector(".profile__title");
@@ -52,10 +52,10 @@ const profileEditModalInputDescription = document.querySelector(
   "[name='description']"
 );
 const addCardModalInputTitle = document.querySelector("[name='name']");
-const addCardModalInputUrl = document.querySelector("[name='url']");
-const previewImage = previewImageModal.querySelector(".modal__preview-image");
+const addCardModalInputUrl = document.querySelector("[name='link']");
+const previewImage = previewImageModal.querySelector(".popup__preview-image");
 const previewDescription = previewImageModal.querySelector(
-  ".modal__preview-description"
+  ".popup__preview-description"
 );
 
 // Event Handlers
@@ -99,10 +99,12 @@ previewImageModalCloseButton.addEventListener("click", () =>
 
 //Functions
 function openModal(modal) {
-  modal.classList.add("modal_opened");
+  modal.classList.add("popup_opened");
+  document.addEventListener("keyup", handleEscUp);
 }
 function closeModal(modal) {
-  modal.classList.remove("modal_opened");
+  modal.classList.remove("popup_opened");
+  document.removeEventListener("keyup", handleEscUp);
 }
 function getCardElement(data) {
   const cardElement = cardTemplate.cloneNode(true);
@@ -129,7 +131,50 @@ function getCardElement(data) {
 }
 function renderCard(cardData, cardListEl) {
   const cardEl = getCardElement(cardData);
-  cardListEl.prepend(cardEl);
+  cardListEl.append(cardEl);
 }
+
+// Closing the Popup by Pressing Esc
+const isEscEvent = (evt, action) => {
+  const activePopup = document.querySelector(".popup_opened");
+  if (evt.key === "Escape") {
+    action(activePopup);
+  }
+};
+
+const handleEscUp = (evt) => {
+  evt.preventDefault();
+  isEscEvent(evt, closeModal);
+};
+
 // Loops
 initialCards.forEach((cardData) => renderCard(cardData, cardListEl));
+
+// Closing the Popup by Clicking on the Overlay
+profileEditModal.addEventListener("mousedown", (evt) => {
+  if (
+    evt.target.classList.contains("popup") ||
+    evt.target.classList.contains("popup__close")
+  ) {
+    closeModal(profileEditModal);
+  }
+});
+
+addCardModal.addEventListener("mousedown", (evt) => {
+  if (
+    evt.target.classList.contains(
+      "popup" || evt.target.classList.contains("popup__close")
+    )
+  ) {
+    closeModal(addCardModal);
+  }
+});
+
+previewImageModal.addEventListener("mousedown", (evt) => {
+  if (
+    evt.target.classList.contains("popup") ||
+    evt.target.classList.contains("popup__close")
+  ) {
+    closeModal(previewImageModal);
+  }
+});
